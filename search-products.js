338b0ccc0c93ce2858e8593c735b457b98144734
RabeyaEty01 <etyrabeya01@gmail.com>
2021-08-11 23:14:33 +0600

    search product

---
+++ search-products.js
@@ -0,0 +1,22 @@
+const products = [
+    { name: 'samsung s3 phone', price: 12000 },
+    { name: 'assus laptop d34', price: 36000 },
+    { name: 'apple smart watch', price: 2500 },
+    { name: 'bashundhara binding paper', price: 100 },
+    { name: 'lg smart phone', price: 22000 },
+    { name: 'old granny land phone', price: 1000 },
+]
+
+function searchProduct(products, searchText) {
+    const matched =[];
+    for (const product of products) {
+        const name = product.name;
+        if (name.indexOf(searchText) != -1) {
+            matched.push(product);
+        }
+
+    }
+    return matched;
+}
+const matched = searchProduct(products, 'phone');
+console.log(matched);
